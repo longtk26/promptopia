7cc0ae517c0e4b3f3306f6436d4d8dda2ae7252b
longtk26 <longtk26@gmail.com>
2023-06-25 10:28:15 +0700

    test production

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,7 +1,9 @@
 import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
 
 export const GET = async (req) => {
     try {
+        await connectToDB();
         const allPrompts = await Prompt.find({}).populate("creator");
 
         return new Response(JSON.stringify(allPrompts), { status: 200 });
